514b58a0bff1f1eabdf183a9c057490a7bc77cf4
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-11-12 14:00:29 +0100

    Add project files.

---
+++ Open-Lab-10.01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Open_Lab_10._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book("The Two Towers", 415);
+            Console.ReadKey();
+        }
+    }
+    class Book
+    {
+        public string title;
+        private int pages;
+        public Book(string ttl, int pgs)
+        {
+            title = ttl;
+            pages = pgs;
+            Console.WriteLine(title + " has " + pages + " pages");
+        }
+    }
+}
+     
